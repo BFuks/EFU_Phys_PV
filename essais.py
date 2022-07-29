d84e04f5f6474549b37ce74f055397d3000014fd
Nicolas Treps <nicolas@macbook-air-1.home>
2022-07-29 09:06:34 +0200

    ajout essais.py pour tests pandas

--- essais.py
+++ essais.py
@@ -8,7 +8,7 @@ Created on Wed Jul 20 09:36:13 2022
 
 import pandas
 
-mondict = {'ET1' : {('nom','nom'):'nom1', ('UE1', 'note1'):[10], ('UE1', 'note2'):[11]}, 'ET2' : {('nom','nom'):'nom2', ('UE1', 'note1'):[12], ('UE1', 'note2'):[13]}}
+mondict = {'ET1' : {('nom','nom'):'nom1', ('UE1', 'note1'):10, ('UE1', 'note2'):11}, 'ET2' : {('nom','nom'):'nom2', ('UE1', 'note1'):12, ('UE1', 'note2'):13}}
 multdf = pandas.DataFrame(mondict)
 multdfT=multdf.transpose()
 #multdfT.to_csv(r'genessai.csv', index = True, header=True)
